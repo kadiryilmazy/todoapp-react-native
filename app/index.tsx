61bc5ee4a6f11996e2400713e27ffdcaaa3759c2
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-05-04 12:35:12 +0300

    add header and search bar components to enhance UI functionality

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { Ionicons } from "@expo/vector-icons";
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { FlatList, Image, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
     const todoData = [
@@ -31,11 +31,40 @@ export default function Index() {
     ];
     return (
         <SafeAreaView style={styles.container}>
-            <Ionicons
-                name="menu"
-                size={24}
-                color={"#333"}
-            ></Ionicons>
+            <View style={styles.header}>
+                <TouchableOpacity
+                    onPress={() => {
+                        alert("clicked");
+                    }}
+                >
+                    <Ionicons
+                        name="menu"
+                        size={24}
+                        color={"#333"}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => {
+                        alert("clicked img");
+                    }}
+                >
+                    <Image
+                        source={{ uri: "https:/xsgames.co/randomusers/avatar.php?g=male" }}
+                        style={{ width: 40, height: 40, borderRadius: 20 }}
+                    />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.searchBar}>
+                <Ionicons
+                    name="search"
+                    size={24}
+                    color={"#333"}
+                />
+                <TextInput
+                    placeholder="Search"
+                    style={styles.searchInput}
+                />
+            </View>
             <FlatList
                 data={todoData}
                 keyExtractor={(item) => item.id.toString()}
@@ -52,7 +81,26 @@ export default function Index() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        paddingHorizontal: 20,
+        backgroundColor: "#f5f5f5",
+    },
+    header: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 20,
+        paddingTop: 20,
+    },
+    searchBar: {
+        flexDirection: "row",
+        backgroundColor: "#fff",
         justifyContent: "center",
         alignItems: "center",
+        padding: 5,
+        borderRadius: 12,
+        gap: 10,
+    },
+    searchInput: {
+        flex: 1,
     },
 });
